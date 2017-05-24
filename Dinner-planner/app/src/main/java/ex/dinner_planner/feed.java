09ea4afb39826bf8afa666680160b83c2b3e0ef6
carlston17 <carlston23238@gmail.com>
2017-05-24 00:21:06 -0400

    created base formats for activities

---
+++ Dinner-planner/app/src/main/java/ex/dinner_planner/feed.java
@@ -0,0 +1,13 @@
+package ex.dinner_planner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
